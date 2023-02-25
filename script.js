70ec89d17244500e3b400750dacaf1e0e92dafa4
robertsergo <111302645+robertsergo@users.noreply.github.com>
2023-02-25 15:13:36 -0500

    code source

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-
 var perso = document.querySelector(".perso");
 var obstacles = document.querySelector(".obstacles");
 
 function sauter(){
-    //alert("bonjour")
-    
+    // function pour faire sauter le personnage
     if(perso.classList != "animation"){
-        perso.classList.add('animation');
+        perso.classList.add('animation'); 
     }
     setTimeout(function(){
-        perso.classList.remove('animation');
-
+        perso.classList.remove('animation'); 
     },500)
-    verification();
 }
 
-//vérifier si l'obstacle touche la personne
-var verification = setInterval(function(){
-    var persoTop = parseInt(window.getComputedStyle(perso).getPropertyValue("top"));
-    var obstaclesLeft = parseInt(window.getComputedStyle(obstacles).getPropertyValue("left"));
+//verifier si  l'obstacle touche le personnage
 
-    if(obstaclesLeft < 20 && obstaclesLeft > 0 && persoTop >= 130){
+var verification = setInterval(function(){
+    var persoTop = parseInt(window.getComputedStyle(perso).getPropertyValue("top")) ;
+    var obstaclesLeft = parseInt(window.getComputedStyle(obstacles).getPropertyValue("left")) ;
+
+    if(obstaclesLeft<20 && obstaclesLeft >0 && persoTop>= 130){
         obstacles.style.animation = "none";
-        alert("perdu")
+        alert("Vous avez perdu")
+
     }
-}, 1)
+
+},1)
